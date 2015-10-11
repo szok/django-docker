c8fa0c102f8f107b7e57bd7321aaed03e3897e1d
Mateusz Kurek <mateusz.kurek@allegrogroup.com>
2015-10-11 15:00:40 +0200

    added prod settings

---
+++ blog/settings/production.py
@@ -0,0 +1,27 @@
+from .base import *
+import os
+
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+
+ADMINS = (
+    ('mk', 'Your email'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'blog'),
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', ''),
+        'PORT': os.environ.get('DB_PORT', ''),
+    }
+}
